f37e69d07b5a0e26226939ce3897863e7bdbded8
Ahmed Mohamed <abdulcartel@gmail.com>
2018-05-15 09:43:45 +0300

    modified css and index and finished on the BL in js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,43 @@
 //business logic
-  var Users {}
+
+//pick random number
+var dice = function () {
+  return Math.floor(Math.random()*6 + 1);
+}
+
+function User(turn) {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.gamer;
+}
+
+User.prototype.firstroll = function(){
+  If(this.roll===1){
+    this.tempscore=0;
+    alert("Your turn is over,sorry!");
+  }else{
+    this.tempscore += this.roll
+  }
+}
+
+User.prototype.hold = function(){
+  this.totalscore += this.tempscore;
+  this.tempscre = 0;
+  alert(this.gamer + "Let the other user play,your turn is up")
+}
+
+//check winner
+
+User.prototype.winner = function(){
+  if(this.totalscore >= 100){
+    alert(this.gamer + "has won the game!");  
+  }
+}
+
+
+//User-Interface
+$(document).ready(function(){
+
+})
